9b7e3ab360b7d71172cc1f27ecc15678988e0517
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-02-28 20:12:36 +0100

    creating concrete classes

---
+++ decorator-pattern/Component/IScanner.cs
@@ -0,0 +1,11 @@
+﻿namespace decorator_pattern.Component
+{
+    public interface IScanner
+    {
+        string FrontImage { get; set; }
+        string BackImage { get; set; }
+        string FaceImageChip { get; set; }
+        string FaceImageViz { get; set; }
+        IScanner GetDocumentInformation();
+    }
+}
---
+++ decorator-pattern/ConcreteComponent/Arh.cs
@@ -0,0 +1,25 @@
+﻿using decorator_pattern.Component;
+
+namespace decorator_pattern.ConcreteComponent
+{
+    public class Arh : IScanner
+    {
+        public string FrontImage { get; set; }
+        public string BackImage { get; set; }
+        public string FaceImageChip { get; set; }
+        public string FaceImageViz { get; set; }
+
+        public Arh(string frontImage, string backImage, string faceImageChip, string faceImageViz)
+        {
+            FrontImage = frontImage;
+            BackImage = backImage;
+            FaceImageChip = faceImageChip;
+            FaceImageViz = faceImageViz;
+        }
+
+        public IScanner GetDocumentInformation()
+        {
+            return this;
+        }
+    }
+}
