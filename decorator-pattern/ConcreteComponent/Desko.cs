fe70b963b2a6e0ffc70a9f1e93c0edcae93b7d77
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-03-02 20:39:30 +0100

    desko scanner concrete component

---
+++ decorator-pattern/ConcreteComponent/Desko.cs
@@ -0,0 +1,20 @@
+﻿using decorator_pattern.Component;
+
+namespace decorator_pattern.ConcreteComponent
+{
+    public class Desko : Scanner
+    {
+        public Desko(string frontImage, string backImage, string faceImageChip, string faceImageViz)
+        {
+            FrontImage = frontImage;
+            BackImage = backImage;
+            FaceImageChip = faceImageChip;
+            FaceImageViz = faceImageViz;
+        }
+
+        public override Scanner GetDocumentInformation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
